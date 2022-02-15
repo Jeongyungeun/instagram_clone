cc1d6926d8a7e78aba28196188728e12e8926e8b
yungeun <yungun3@gmail.com>
2022-02-16 00:48:23 +0900

    24. custom selection button in profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/constant/common_size.dart';
+import 'package:instagram_clone/widgets/pofile_body.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -13,65 +13,13 @@ class ProfileScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _appBar(),
-            Expanded(
-              child: CustomScrollView(
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildListDelegate([
-                      _userName(),
-                      _userBio(),
-                      _editProfileButton(),
-                    ],),
-                  ),
-                ],
-              ),
-            ),
+            ProfileBody()
           ],
         ),
       ),
     );
   }
 
-  Padding _editProfileButton() {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: common_gap),
-      child: SizedBox(
-        height: 30,
-        child: OutlinedButton(
-            style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                side: BorderSide(width: 3.0, color: Colors.black87)),
-            onPressed: () {},
-            child: Text(
-              'Edit Profile',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
-            )),
-      ),
-    );
-  }
-
-  Widget _userName() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: common_gap),
-      child: Text(
-        'userName',
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  Widget _userBio() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: common_gap),
-      child: Text(
-        'This is what I believe',
-        style: TextStyle(fontWeight: FontWeight.w400),
-      ),
-    );
-  }
 
   Row _appBar() {
     return Row(
---
+++ lib/widgets/pofile_body.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+import '../constant/common_size.dart';
+
+class ProfileBody extends StatefulWidget {
+
+  ProfileBody({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileBody> createState() => _ProfileBodyState();
+}
+
+class _ProfileBodyState extends State<ProfileBody> {
+  bool selectedLeft = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                _userName(),
+                _userBio(),
+                _editProfileButton(),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: IconButton(
+                        onPressed: (){
+                          selectedLeft = true;
+                          setState(() {
+                          });
+                        },
+                        icon: ImageIcon(
+                          AssetImage('assets/image/grid.png'),
+                          color: selectedLeft? Colors.black:Colors.black26,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: (){
+                          selectedLeft = false;
+                          setState(() {
+                          });
+                        },
+                        icon: ImageIcon(
+                          AssetImage('assets/image/saved.png'),
+                          color: selectedLeft? Colors.black26:Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding _editProfileButton() {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: common_gap, vertical: common_gap),
+      child: SizedBox(
+        height: 30,
+        child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                side: BorderSide(width: 3.0, color: Colors.black87)),
+            onPressed: () {},
+            child: Text(
+              'Edit Profile',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+            )),
+      ),
+    );
+  }
+
+  Widget _userName() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: common_gap),
+      child: Text(
+        'userName',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _userBio() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: common_gap),
+      child: Text(
+        'This is what I believe',
+        style: TextStyle(fontWeight: FontWeight.w400),
+      ),
+    );
+  }
+}
